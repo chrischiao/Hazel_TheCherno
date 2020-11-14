6caf51739a2fbc896416e4e64985a0ed7d807e9a
chrischiao <3391622007@qq.com>
2020-11-14 23:52:15 +0800

    Entry Point

---
+++ Hazel/Hazel/src/Hazel/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "core.h"
+
+namespace Hazel{
+
+	class HAZEL_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+		void Run();
+	};
+
+	// To be defined in CLIENT
+	Application* CreateApplication();
+}
+
+
--- Hazel/Hazel/src/Test.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Hazel {
-
-	__declspec(dllexport) void Print();
-
-}
