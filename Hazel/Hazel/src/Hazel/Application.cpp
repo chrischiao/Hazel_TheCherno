5b2a8d3a589e178003500a828f83e1448b9c00f8
chrischiao <3391622007@qq.com>
2020-12-13 21:55:28 +0800

    Key and Mouse Codes

--- Hazel/Hazel/src/Hazel/Application.cpp
+++ Hazel/Hazel/src/Hazel/Application.cpp
@@ -71,9 +71,6 @@ namespace Hazel
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
 
-			auto [x, y] = Input::GetMousePosition();
-			HZ_CORE_TRACE("{0}, {1}", x, y);
-
 			m_Window->OnUpdate();
 		}
 	}
